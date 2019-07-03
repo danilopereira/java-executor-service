5a419ec41a01f2dac29e8f6a98ad01353f700e83
danilopereira <dansilva13@gmail.com>
2019-07-03 18:03:22 -0300

    Executor Service using callable

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import com.danilopereira.callable.ExecutorServiceCallableSample;
 import com.danilopereira.runnable.ExecutorServiceRunnableSample;
+
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorServiceRunnableSample executorServiceRunnableSample = new ExecutorServiceRunnableSample();
         executorServiceRunnableSample.execute();
         executorServiceRunnableSample.submite();
+
+        ExecutorServiceCallableSample executorServiceCallableSample = new ExecutorServiceCallableSample();
+        executorServiceCallableSample.submit();
 
 
     }
--- src/com/danilopereira/callable/ExecutorServiceCallableSample.java
+++ src/com/danilopereira/callable/ExecutorServiceCallableSample.java
@@ -1,6 +1,38 @@
 package com.danilopereira.callable;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorServiceCallableSample {
 
+    public void submit() throws ExecutionException, InterruptedException {
 
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(2);
+        try{
+            final Future<String> task1 = fixedThreadPool.submit(doSomethingExpensiveInCallable(Thread.currentThread().getName()));
+            System.out.println(task1.get());
+
+            final Future<String> task2 = fixedThreadPool.submit(doSomethingExpensiveInCallable(Thread.currentThread().getName()));
+            System.out.println(task2.get());
+            while(!task1.isDone() && !task2.isDone()){
+                Thread.sleep(1000);
+            }
+        }finally {
+            fixedThreadPool.shutdown();
+            fixedThreadPool.shutdownNow();
+        }
+    }
+
+    private Callable<String> doSomethingExpensiveInCallable(String threadName) {
+        Callable<String> callableTask = () ->{
+            TimeUnit.MILLISECONDS.sleep(3000);
+            return String.format("Callable Task in thread %s", threadName);
+        };
+
+        return callableTask;
+    }
 }
